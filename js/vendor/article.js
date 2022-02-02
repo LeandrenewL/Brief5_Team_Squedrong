417485269b8aeba674506f87e1fcdf229e9718f0
lichagrab <nathaliegrabowski.dev@gmail.com>
2022-02-02 15:08:18 +0100

    ajout js

---
+++ js/vendor/article.js
@@ -0,0 +1,2 @@
+const url ="http://localhost:1337/api/articles?populate=*"
+ alert("fichier bien creer");
---
+++ js/vendor/index.js
@@ -0,0 +1,22 @@
+let bouton = document.getElementById("hamburger");
+
+bouton.onclick = function() {
+  let menu = document.getElementById("menu");
+
+   console.log(bouton.classList);
+  
+  if (bouton.classList.contains("inactive")){ // Si le contenu est le hamburger :
+    bouton.innerHTML = "&#x2715;";
+    bouton.classList.add("active");
+    bouton.classList.remove("inactive");
+    menu.classList.add("menu_active");
+  }
+  else{
+    bouton.innerHTML = "&#9776;";
+    bouton.classList.add("inactive");
+    bouton.classList.remove("active");
+    menu.classList.remove("menu_active");
+  }
+}
+ const url ="http://localhost:1337/api/articles?populate=*"
+ alert("fichier bien creer");
