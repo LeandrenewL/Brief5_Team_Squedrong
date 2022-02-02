0c7eed6f0a926c4243b8fa8f0482f49ee706a921
BUGHIN Léandre <bughin.leandre@gmail.com>
2022-02-02 15:41:28 +0100

    js exo

--- js/vendor/article.js
+++ js/vendor/article.js
@@ -1,2 +1,11 @@
-const url ="http://localhost:1337/api/articles?populate=*"
+const url ="http://localhost:1337/api/articles?populate=*";
+
+
  alert("fichier bien creer");
+
+ function getArticles(){
+     fetch(url)
+     .then(response => response.json())
+     .then(reponse => printArticles(reponse))
+     .catch(error => alert("Erreur : " + error))
+ }
