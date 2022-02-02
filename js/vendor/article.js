c814d30c1e1cba9dd8083d6b4f773ea4371c99ca
BUGHIN Léandre <bughin.leandre@gmail.com>
2022-02-02 16:17:52 +0100

    exo js continue

--- js/vendor/article.js
+++ js/vendor/article.js
@@ -3,6 +3,16 @@ const url ="http://localhost:1337/api/articles?populate=*";
 
  alert("fichier bien creer");
 
+ function printArticles(data){
+     let main = document.getElelementById("main_grille")
+
+     for (let article of data.data){
+        let div = main_grille.appendchild(document.createElement("div"));
+        div.classList.add("box1")
+        let img = div.appendchild(document.createElement("img"))
+     }
+ }
+
  function getArticles(){
      fetch(url)
      .then(response => response.json())
