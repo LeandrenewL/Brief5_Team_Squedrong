e9fe4c51a5177a5a528f11f0143b826afe3019ba
BUGHIN Léandre <bughin.leandre@gmail.com>
2022-02-03 10:58:27 +0100

    Js exo fin

--- js/vendor/article.js
+++ js/vendor/article.js
@@ -1,24 +1,24 @@
 const url ="http://localhost:1337";
-const urlarticle ="/api/articles?populate=*"
+const urlArticle ="/api/articles?populate=*"
 
- alert("fichier bien creer");
 
  function printArticles(data){
-     let main = document.getElelementById("main_grille")
+     let main = document.getElementById("main_grille");
 
      for (let article of data.data){
-        let div = main_grille.appendchild(document.createElement("div"));
+        let div = main_grille.appendChild(document.createElement("div"));
         div.classList.add("box1")
-        let img = div.appendchild(document.createElement("img"))
-        img.setAttribute("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
-        let p = div.appendchild(document.createElement("p"))
-        p.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
-        let h3 = div.appendchild(document.createElement("h3"))
-        h3.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
-        let h4 = div.appendchild(document.createElement("h4"))
-        let a = h4.appendchild(document.createElement("a"))
-        h4.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
-        a.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
+        let img = div.appendChild(document.createElement("img"))
+        img.setAttribute("src", url+article.attributes.illustration.data.attributes.formats.medium.url);
+        let p = div.appendChild(document.createElement("p"))
+        p.innerText = article.attributes.publishedAt;
+        let h3 = div.appendChild(document.createElement("h3"))
+        h3.innerText = article.attributes.title;
+        let p2 = div.appendChild(document.createElement("p"))
+        p2.innerText = article.attributes.resume;
+        let h4 = div.appendChild(document.createElement("h4"))
+        let a = h4.appendChild(document.createElement("a"))
+        a.innerText="En Savoir Plus";
      }
  }
 
@@ -26,5 +26,7 @@ const urlarticle ="/api/articles?populate=*"
      fetch(url+urlArticle)
      .then(response => response.json())
      .then(reponse => printArticles(reponse))
-     .catch(error => alert("Erreur : " + error))
+     //.catch(error => alert("Erreur : " + error))
  }
+
+ getArticles();
