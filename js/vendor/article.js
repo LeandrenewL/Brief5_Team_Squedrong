39d8fe2e9ee10b30c4d6dc2003bf06711e468dc9
BUGHIN Léandre <bughin.leandre@gmail.com>
2022-02-03 10:05:47 +0100

    article js ajout

--- js/vendor/article.js
+++ js/vendor/article.js
@@ -1,5 +1,5 @@
-const url ="http://localhost:1337/api/articles?populate=*";
-
+const url ="http://localhost:1337";
+const urlarticle ="/api/articles?populate=*"
 
  alert("fichier bien creer");
 
@@ -10,11 +10,20 @@ const url ="http://localhost:1337/api/articles?populate=*";
         let div = main_grille.appendchild(document.createElement("div"));
         div.classList.add("box1")
         let img = div.appendchild(document.createElement("img"))
+        img.setAttribute("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
+        let p = div.appendchild(document.createElement("p"))
+        p.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
+        let h3 = div.appendchild(document.createElement("h3"))
+        h3.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
+        let h4 = div.appendchild(document.createElement("h4"))
+        let a = h4.appendchild(document.createElement("a"))
+        h4.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
+        a.innerText("src", url+article.attributes.illustation.data.attributes.formats.medium.url);
      }
  }
 
  function getArticles(){
-     fetch(url)
+     fetch(url+urlArticle)
      .then(response => response.json())
      .then(reponse => printArticles(reponse))
      .catch(error => alert("Erreur : " + error))
